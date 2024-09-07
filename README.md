3f49e729fa4a5fda87e76ab1208594693807072a
Shun <s.tsuchiya.business@gmail.com>
2024-09-07 21:29:31 +0900

    update message and readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Langcheck CLI
+
+This is a command line interface for the [langcheck](https://github.com/citadel-ai/langcheck/tree/main)
+
+## Warning
+
+1. This is not official.
+2. This is a prototype. It is not stable and may not work as expected.
+
+## Installation
+
+```shell
+git clone git@github.com:shunsock/langcheck_cli.git
+cd langcheck_cli
+uv run langcheck --help
+```
+
+## Usage
+
+```shell
+Usage: uv run langcheck_cli [options] [sub_commend] ...
+Options:
+    -h, --help      show help message
+
+Subcommands:
+    metrics:
+        show metrics of the target
+        
+        -n, --name          metrics
+        -f, --file          file path
+        -u, --upper-than    show metrics of the target that is upper than the value
+        -l, --lower-than    show metrics of the target that is lower than the value
+```
--- src/langcheck_cli/controller/help_message.py
+++ src/langcheck_cli/controller/help_message.py
@@ -7,16 +7,17 @@ class HelpMessage(Controller):
 
     def run(self) -> None:
         message = """
-        Usage: uv run langcheck_cli [options] [sub_commend] ...
-        Options:
-            -h, --help      show help message
+Usage: uv run langcheck_cli [options] [sub_commend] ...
+Options:
+    -h, --help      show help message
+
+Subcommands:
+    metrics:
+        show metrics of the target
         
-        Subcommands:
-            metrics         show metrics of the target
-                Options for metrics:
-                    -n, --name          metrics
-                    -f, --file          file path
-                    -u, --upper-than    show metrics of the target that is upper than the value
-                    -l, --lower-than    show metrics of the target that is lower than the value
-        """
+        -n, --name          metrics
+        -f, --file          file path
+        -u, --upper-than    show metrics of the target that is upper than the value
+        -l, --lower-than    show metrics of the target that is lower than the value
+"""
         print(message)
